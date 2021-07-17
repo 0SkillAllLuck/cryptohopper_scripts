e9713cc74c788498411f9c200a2239d5435e490b
Cedric Lewe <43417046+0SkillAllLuck@users.noreply.github.com>
2021-07-17 16:59:18 +0200

    Improve code

--- coin-list-import-export.user.js
+++ coin-list-import-export.user.js
@@ -2,12 +2,13 @@
 // @name         CryptoHopper Coin List Import / Exporter
 // @namespace    https://github.com/0SkillAllLuck/cryptohopper_scripts
 // @updateUrl    https://github.com/0SkillAllLuck/cryptohopper_scripts/raw/main/coin-list-import-export.user.js
-// @version      0.1
+// @version      0.2
 // @description  Add an import / export option for the coin list on the Crypto Hopper Hopper config page
 // @author       0SkillAllLuck
 // @match        https://www.cryptohopper.com/config
 // @icon         https://www.google.com/s2/favicons?domain=www.cryptohopper.com
 // @grant        GM.setClipboard
+// @grant        GM.getClipboard
 // ==/UserScript==
 
 (function () {
@@ -15,38 +16,48 @@
 
     function exportCoinList() {
         GM.setClipboard(jQuery('#allowed_coins').val().join(','));
-        swal({ title: 'Success', text: 'Coinlist exported!', type: 'success' });
+        swal({
+            type: 'success',
+            title: 'Coinlist exported',
+            text: "Coinlist exported to clipboard",
+            showConfirmButton: false,
+            timer: 1250,
+            timerProgressBar: true,
+        })
     }
 
     function importCoinList() {
         swal({
             title: 'Import Coinlist',
-            input: 'textarea',
             text: 'Input your coinlist, comma seperated',
-            inputPlaceholder: 'BTC,ETH,XRP etc.',
             showCancelButton: true,
+            input: 'textarea',
+            inputPlaceholder: 'BTC,ETH,XRP etc.'
         }).then((result) => {
             jQuery('#allowed_coins').val(result.value.split(",")).change();
-            return swal({ title: 'Success', text: 'Coinlist imported!', type: 'success' });
-        }).then((result) => {
+        }).then(() => {
+            swal({
+                type: 'success',
+                title: 'Coinlist imported',
+                showConfirmButton: false,
+                timer: 1250,
+                timerProgressBar: true,
+            })
+        }).then(() => {
             saveConfig();
         });
     }
 
-    function addElements() {
-        const exportButton = jQuery('<a href="#"><i class="fa fa-upload m-r-5"></i> Export Coinlist</a>');
-        const exportButtonListItem = jQuery('<li></li>');
-        exportButton.on('click', () => exportCoinList());
-        exportButtonListItem.append(exportButton);
-
-        const importButton = jQuery('<a href="#"><i class="fa fa-download m-r-5"></i> Import Coinlist</a>');
-        const importButtonListItem = jQuery('<li></li>');
-        importButton.on('click', () => importCoinList());
-        importButtonListItem.append(importButton);
-
-        jQuery('.page-title-box .dropdown-menu').append(exportButtonListItem);
-        jQuery('.page-title-box .dropdown-menu').append(importButtonListItem);
+    function createButton(icon, text, onClick) {
+        const button = jQuery('<a href="#"><i class="fa fa-' + icon + ' m-r-5"></i> ' + text + '</a>');
+        const listItem = jQuery('<li></li>');
+        button.on('click', onClick);
+        listItem.append(button);
+        return listItem;
     }
 
-    jQuery(document).ready(() => addElements());
+    jQuery(document).ready(() => {
+        jQuery('.page-title-box .dropdown-menu').append(createButton('upload', 'Export Coinlist', () => exportCoinList()));
+        jQuery('.page-title-box .dropdown-menu').append(createButton('download', 'Import Coinlist', () => importCoinList()));
+    });
 })();
